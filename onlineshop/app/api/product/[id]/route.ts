52a6bbadaad0765e453d397f238488b321b310eb
AliHosaam <ah5778902@gmail.com>
2024-02-17 23:11:55 +0300

    Test

--- onlineshop/app/api/product/[id]/route.ts
+++ onlineshop/app/api/product/[id]/route.ts
@@ -2,16 +2,17 @@ import { getCurrentUser } from "@/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/libs/prismadb";
 
-export async function DELETE({ params }: { params: { id: string } }) {
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   const currentUser = await getCurrentUser();
 
   if (!currentUser || currentUser.role !== "ADMIN") return NextResponse.error;
 
-  const id = params.id;
-
   const product = await prisma.product.delete({
     where: {
-      id: id,
+      id: params.id,
     },
   });
 
